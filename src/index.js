b43abeb0b1c6692c42bb6201382a8600665d2fc6
HaZckerBRO <32968660+HaZckerBRO@users.noreply.github.com>
2020-11-24 15:52:58 +0300

    base config (#1)
    * installed webpack and webpack-cli
    
    * finished base config
    
    * changed page title. Removed material-icons stylesheet link

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/main.sass'
+import 'antd/dist/antd.css'
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
